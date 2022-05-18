83847532c11fc8e6ab15b7fe9abd6658b34ed45d
Júlio Adler <julioader@gmail.com>
2022-05-18 13:26:25 -0300

    atualização exercícios

---
+++ fundamentos/bloco-05-Logica/bonus/bonus1/script.js
@@ -0,0 +1,9 @@
+let num = 10;
+let arrayPontos = '';
+
+for (index1 = 0; index1 < num; index1 +=1){
+  arrayPontos +=('*');
+}
+for (index = 0; index < num; index += 1){
+  console.log(arrayPontos);
+}
