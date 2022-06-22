f2c0bee42dac3ea26257fbee914121d00acf7c53
Júlio Adler <julioader@gmail.com>
2022-06-22 16:06:46 -0300

    Exercicios dia 1 - bloco 8

---
+++ fundamentos/bloco-08-Higher-Order-Functions/dia-1-introducao-a-higher-order-functions/index.js
@@ -0,0 +1,7 @@
+const acorda = () => 'Acordando!!';
+const cafe = () => 'Bora tomar café!!';
+const dormir = () => 'Partiu dormir!!';
+
+const doingThings = (thing) => thing();
+
+console.log(doingThings(acorda));
